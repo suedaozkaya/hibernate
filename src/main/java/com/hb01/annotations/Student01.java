b81015313ce7a03b761b7e2d93ec6fa9366b9604
Süeda Özkaya <sozkaya2635@gmail.com>
2022-09-25 09:22:28 +0300

    25.09.22

---
+++ src/main/java/com/hb01/annotations/Student01.java
@@ -0,0 +1,68 @@
+package com.hb01.annotations;
+
+import javax.persistence.*;
+
+//Bu annotationu koyduğumuz sınıf, veritabanında bir tabloya karşılık gelecek
+@Entity
+//Eğer oluşacak tablo ismini değiştirmek istenirse name attribute ile bir tablo ismi verilir
+@Table(name = "t_student01")
+public class Student01 {
+
+
+    @Id //primary key oluşmasını sağlıyor.
+    //@Column (name = "std_id")
+    private int id;
+
+
+    //lenght default=255
+    //nullable default=true
+    //unique default=false
+    //Column annotationu zorunlu değil ancak (tablodaki ismi) customize edebilmek için gerekli
+    @Column(name = "student_name",length = 100, nullable = false,unique = true)
+    private String name;
+
+
+    //veritabanı tablosunda grade adında bir column oluşturulmaz
+    //örneğin bazı değerleri yalnızca kod içerisinde tutmak istiyoruz, veri tabanında olsun istemiyoruz
+    //@Transient
+    private int grade;
+
+
+    //large object(lob) ile büyük boyutlu datalar tutulabilir.(image,file)
+    //@Lob
+    //private byte[] image;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student01{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
---
+++ src/main/java/com/hb06/uni_onetomany/RunnerFetch06.java
@@ -0,0 +1,71 @@
+package com.hb06.uni_onetomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerFetch06 {
+
+    public static void main(String[] args) {
+
+        Configuration con=new Configuration().configure("hibernate.cfg.xml").
+                addAnnotatedClass(Student06.class).addAnnotatedClass(Book06.class);
+
+        SessionFactory sf= con.buildSessionFactory();
+        Session session= sf.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+
+//		Student06 student = session.get(Student06.class, 1001);
+//		System.out.println(student);
+
+
+
+/*
+        //bir book id ile kitabı getirme
+		String hqlQuery1="FROM Book06 b where b.id=101";
+		Book06 book= session.createQuery(hqlQuery1,Book06.class).uniqueResult();
+		System.out.println(book);
+*/
+
+
+
+/*
+        //bir öğrenci id ile kitapları çekme
+		Student06 student = session.get(Student06.class, 1001);
+		student.getBookList().forEach(b->System.out.println(b));
+ */
+
+
+
+//		Book06 book=session.get(Book06.class, 101);
+//		System.out.println(book);
+
+
+
+/*
+		String hqlQuery1="FROM Book06 b where b.id=101";
+     	Book06 book= session.createQuery(hqlQuery1,Book06.class).uniqueResult();
+		System.out.println(book);
+*/
+
+
+
+/*
+        //bir öğrencinin kitaplarını öğrenci id'ye göre getirme
+		String hqlQuery2 = "SELECT b.id, b.name FROM Student06 s INNER JOIN s.bookList b WHERE s.id=1001";
+		List<Object[]> resultList1 = session.createQuery(hqlQuery2).getResultList();
+
+		resultList1.forEach(oa->System.out.println(Arrays.toString(oa)));
+*/
+
+
+
+        tx.commit();
+        session.close();
+        sf.close();
+    }
+
+}
